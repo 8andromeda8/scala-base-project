0ed68cd1c00e477292a1fe90fad4348a70929710
Alexanda Avendano <alexandraavendano@seven4n.com>
2015-08-10 10:11:58 -0500

    Creacion de clase Calculator

---
+++ src/main/scala/co/rc/Calculator.scala
@@ -0,0 +1,11 @@
+package co.rc
+
+
+class Calculator(x:Int, y:Int){
+
+  val a: Int = x
+  val b: Int = y
+
+
+
+}
--- src/main/scala/co/rc/Main.scala
+++ src/main/scala/co/rc/Main.scala
@@ -2,8 +2,4 @@ package co.rc
 
 object Main extends App {
 
-  def hello( name: String ): String = {
-    s"Hello $name"
-  }
-
 }
